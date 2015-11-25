2da03548c40657a38b0db46de3508980cf2604bf
liuhu <hooglecrystal@126.com>
2015-11-25 14:07:22 +0800

    中楼层 面积

--- scripts/multi_genetic.py
+++ scripts/multi_genetic.py
@@ -3,14 +3,14 @@ import os
 
 def genetic_one(index):
   if index%2 == 0:
-        sys_str='python genetic_price.py 2000 0.6 0.6 100 2>&1 > .liuqcprice.out%04d &'%index
+        sys_str='python genetic_price.py 2000 0.6 0.6 500 2>&1 > .liuqcprice.out%04d &'%index
   else:
-        sys_str='python genetic_period.py 2000 0.6 0.6 100 2>&1 > .liuqcperiod.out%04d &'%index
+        sys_str='python genetic_period.py 2000 0.6 0.6 500 2>&1 > .liuqcperiod.out%04d &'%index
   os.system(sys_str)
 
 if __name__ == '__main__':
-    pool = Pool(processes=4)
-    num = 4
+    pool = Pool(processes=10)
+    num = 10
     result = pool.map(genetic_one, range(num))
     print 'before close'
     pool.close()
